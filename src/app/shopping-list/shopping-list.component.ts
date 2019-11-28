9577ebf512cea99a384872b08a5964b3e3eae673
Brany Suero <branysuero@gmail.com>
2019-11-28 16:15:55 -0500

    fixing the ingredient component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Ingredient} from '../shared/ingredient.model';
+
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("totone", 2),
+    new Ingredient("apple", 3)
+  ];
+  constructor() {}
 
-  ingredients = [];
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
