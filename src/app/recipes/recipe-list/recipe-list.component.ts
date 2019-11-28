e006af9a0c965a93e16c039194f65fac44fdc890
Brany Suero <branysuero@gmail.com>
2019-11-28 15:44:42 -0500

    added an array of recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "a test",
+      // tslint:disable-next-line:max-line-length
+      "https://cdn.apartmenttherapy.info/image/upload/v1564775676/k/Photo/Recipes/2019-08-how-to-juiciest-turkey-meatballs/How-to-Make-the-Best-Juiciest-Turkey-Meatballs_055.jpg"
+    ),
+    new Recipe(
+      "A test recipe",
+      "a test",
+      // tslint:disable-next-line:max-line-length
+      "https://cdn.apartmenttherapy.info/image/upload/v1564775676/k/Photo/Recipes/2019-08-how-to-juiciest-turkey-meatballs/How-to-Make-the-Best-Juiciest-Turkey-Meatballs_055.jpg"
+    )
+  ];
+  constructor() {}
 
-  recipes = [];
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
